2b70fa3313ecab4d18c41fc520e98c21c9520341
Lawrence <61335367+pnyaa@users.noreply.github.com>
2026-04-07 23:39:20 +0100

    Build: Option for static and dynamic build of cstl. In reality this just makes a target show up in vs2022

---
+++ source/plushie_cstl_impl.c
@@ -0,0 +1,5 @@
+// Source file which is only used if you're building a static or dynamic lib 
+// version of plushie_cstl for reuse.
+//
+// simply just includes each source file with it's required defines to 
+// generate the implementation
